a9a1d24d7c7168b86f26d932780896fea3f10eec
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2023-10-19 18:34:28 +0700

    init

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>P</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-for="(anggota, index) in datas" :key="anggota.id">
+        <div>{{ index + 1 }}</div>
+        <div>{{ anggota.nama }}</div>
+        <div>{{ anggota.kategori }}</div>
+        <div>{{ anggota.keperluan }}</div>
+        <div>{{ anggota.tanggal }}</div>
+    </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const datas = ref([])
+
+async function getData() {
+    const { data, error } = await supabase
+    .from("anggota")
+    .select()
+    if (error) throw error
+    if (data) {
+        console.log(data)
+        datas.value = data
+    } 
+}
+
+onMounted(() => {
+    getData()
+})
+</script>
+
+<style scoped>
+
+</style>
