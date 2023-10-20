ca3d046010559bdb63ead9f7fe204da2ca900d95
Reza Sapitra <113533664+reezyee@users.noreply.github.com>
2023-10-20 14:26:22 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        <p>P</p>
+        <div class="nav-container">
+            <img src="@/assets/images/companylogo.png" class="company"/>
+            <div class="headerText">
+                <h1>PERPUSTAKAAN DIGITAL</h1>
+                <h4>SMK NEGERI 4 TASIKMALAYA</h4>
+            </div>
+            <img src="@/assets/images/perpusnasional.png" class="perpusnasional"/>
+            <img src="@/assets/images/logosmk.png" class="smk"/>
+        </div>
     </div>
 </template>
 
@@ -9,5 +17,60 @@
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bungee&display=swap');
 
+*{
+margin: 0;
+}
+.nav-container {
+    background-color:#413304;
+    width: 100%;
+    height: 150px;
+    display: flex;
+    justify-content: space-evenly;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+}
+
+.company {
+    width: 257.178px;
+    height: 89px;
+    padding: 30px;
+}
+
+.perpusnasional{
+    width: 150.104px;
+    height: 152px;
+}
+.smk{
+    width: 111.077px;
+    height: 126px;
+    padding: 20px;
+}
+.headerText {
+    color: #EFDE79;
+    text-align: center;
+    font-family: '';
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 50;
+    line-height: normal;
+    display: flex;;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-family: 'Bungee', sans-serif;
+}
+
+h4{
+    color: #FFF;
+    text-align: center;
+    font-family: 'Bungee';
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 50;
+    line-height: normal;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,47 @@
 <template>
+    <div class="content">
+        <form @submit.prevent="registerAnswer">
+            <h1>Isi Data Pengunjung</h1>
+            <p>
+                <label for="name">Name</label>
+                <input
+                id="name"
+                v-model="name"
+                type="text"
+                name="name"
+                >
+            </p>
+            <p>
+                <label for="name">Kategori</label>
+                <input
+                id="name"
+                v-model="name"
+                type="text"
+                name="name"
+                >
+            </p>
+            <p>
+                <label for="name">Keperluan</label>
+                <input
+                id="name"
+                v-model="name"
+                type="text"
+                name="name"
+                >
+            </p>
+            <p>
+                <label for="name">DD/MM/YYYY</label>
+                <input
+                id="name"
+                v-model="name"
+                type="date"
+                name="name"
+                >
+            </p>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+    
     <div v-for="(anggota, index) in datas" :key="anggota.id">
         <div>{{ index + 1 }}</div>
         <div>{{ anggota.nama }}</div>
@@ -29,5 +72,34 @@ onMounted(() => {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bungee&display=swap');
 
+.content {
+    background-color: #A59663;
+    width: 100%;
+    height: 1024px;
+    margin-top: 150px;
+    left: 0;
+    right: 0;
+
+}
+.slm{
+    color: #FFF;
+    text-align: center;
+    font-family: 'Bungee', sans-serif;
+    font-size: 64px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.idk {
+    color: #FFF;
+    text-align: center;
+    font-family: 'Bungee', sans-serif;
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
 </style>
